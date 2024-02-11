593854152db71533358bc711c8795857d2e02b97
Michail Kalogeras <89188677+MK-XXXVII@users.noreply.github.com>
2024-02-11 20:05:25 +0100

    Hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,6 +5,7 @@ import type {
   Media,
   Video as MediaVideo,
 } from '@shopify/hydrogen/storefront-api-types';
+import Marquee from 'react-fast-marquee';
 
 import type {CollectionContentFragment} from 'storefrontapi.generated';
 import {Heading, Text, Link} from '~/components';
@@ -33,11 +34,11 @@ export function Hero({
     <Link to={`/collections/${handle}`}>
       <section
         className={clsx(
-          'relative justify-end flex flex-col w-full max-h-[40em] max-w-full md:max-w-[120em] mx-auto overflow-hidden',
+          'relative justify-end flex flex-col max-h-[50rem] md:max-h-[70rem] w-full mx-auto overflow-hidden',
           top && '',
           height === 'full'
             ? 'h-screen'
-            : 'aspect-[4/5] sm:aspect-square md:aspect-[5/4] lg:aspect-[3/2] xl:aspect-[2/1]',
+            : 'aspect-[4/5] sm:aspect-square md:aspect-[5/4] lg:aspect-[3/2] xl:aspect-[5/2]',
         )}
       >
         <div className="absolute inset-0 grid flex-grow grid-flow-col pointer-events-none auto-cols-fr -z-10 content-stretch overflow-clip">
@@ -64,18 +65,51 @@ export function Hero({
             </div>
           )}
         </div>
-        <div className="flex flex-col items-baseline justify-between gap-4 px-6 py-8 sm:px-8 md:px-12 bg-gradient-to-t dark:from-contrast/60 dark:text-primary from-primary/60 text-contrast">
+        <div className="flex flex-col justify-between gap-0 pb-12 bg-gradient-to-t dark:from-contrast/60 dark:text-primary from-primary/60 text-contrast">
           {heading?.value && (
-            <Heading format as="h2" size="display" className="max-w-md">
-              {heading.value}
-            </Heading>
+            <div className="flex items-center justify-center bg-gradient w-full h-20 border-t-4 border-primary">
+              <Marquee speed={150} gradient={false} pauseOnHover>
+                <Heading
+                  format
+                  as="h2"
+                  size="display"
+                  className="max-w-full overflow-hidden py-8"
+                >
+                  {heading.value}
+                </Heading>
+              </Marquee>
+            </div>
           )}
           {byline?.value && (
-            <Text format width="narrow" as="p" size="lead">
-              {byline.value}
-            </Text>
+            <div className="flex items-center justify-center bg-gradient w-full h-12 border-b-4 border-primary">
+              <Marquee speed={100} gradient={false} pauseOnHover>
+                <Text
+                  format
+                  width="narrow"
+                  as="p"
+                  size="lead"
+                  className="max-w-full overflow-hidden py-4"
+                >
+                  {byline.value}
+                </Text>
+              </Marquee>
+            </div>
           )}
-          {cta?.value && <Text size="lead">{cta.value}</Text>}
+          <hr className="my-4 h-0.5 border-t-0" />
+          {cta?.value && (
+            <div className="px-4">
+              <button
+                type="button"
+                className="
+                w-full bg-primary text-contrast hover:text-contrast border-4 
+                border-contrast hover:bg-blue-green focus:ring-4 focus:outline-none 
+                focus:ring-blue-green rounded-full px-8 py-6 text-center mb-2 
+                dark:text-contrast dark:hover:bg-blue-green dark:focus:ring-lilac"
+              >
+                <Text size="lead">{cta.value}</Text>
+              </button>
+            </div>
+          )}
         </div>
       </section>
     </Link>
